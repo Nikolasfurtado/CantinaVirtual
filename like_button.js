7dc5619737fb079716d8133a82fa375610e26728
Nikolas Furtado <105133764+Nikolasfurtado@users.noreply.github.com>
2022-06-01 22:22:21 -0400

    new features added
    starting menu composition with js for marking actual and future date for menu.

--- like_button.js
+++ like_button.js
@@ -1,4 +1,23 @@
-// ... o código inicial que você copiou ...
+function puxa() {
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+    {
+        dayName = new Array("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado");
+        monName = new Array("janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro");
+        now = new Date;
+    }
+
+    var today = document.getElementById("today");
+    today.innerHTML = dayName[now.getDay()];
+
+    var tomorrow = document.getElementById("tomorrow");
+    tomorrow.innerHTML = dayName[now.getDay() + 1];
+
+
+
+    //("Hoje é " + now.getDay() + ", " + now.getDate() + " de " + now.getMonth() + " de " + now.getFullYear());
+
+
+    ////html para ajustar
+    //test.innerHTML = ("<h1> Hoje é " + dayName[now.getDay()] + ", " + now.getDate() + " de " + monName[now.getMonth()] + " de " + now.getFullYear() + ". </h1>");
+
+}
